22255cc43cbbdbef5fd4c56757857b3b42d89fb5
mohammadyaksafu <mohammadyaksafu@gmail.com>
2025-02-12 20:29:01 +0600

    Create slider for emargency contract

--- lib/widgets/home_widgets/emergencies/police_emergency.dart
+++ lib/widgets/home_widgets/emergencies/police_emergency.dart
@@ -5,8 +5,25 @@ class PoliceEmergency extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, bottom: 5),
+      child: Card(
+        elevation:5 ,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Container(
+          height: 180,
+          width: MediaQuery.of(context).size.width*0.7,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(colors: [
+              Colors.green,
+              Colors.limeAccent
+            ])
+          ),
+        ),
+      ),
     );
   }
 }
