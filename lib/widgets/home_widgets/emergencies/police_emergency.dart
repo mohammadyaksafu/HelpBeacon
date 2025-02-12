bddb76eb794a557ed1b441d015a7263d4e549349
mohammadyaksafu <mohammadyaksafu@gmail.com>
2025-02-12 22:46:46 +0600

    Police Emergency  Create For the user

--- lib/widgets/home_widgets/emergencies/police_emergency.dart
+++ lib/widgets/home_widgets/emergencies/police_emergency.dart
@@ -17,10 +17,64 @@ class PoliceEmergency extends StatelessWidget {
           width: MediaQuery.of(context).size.width*0.7,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(colors: [
-              Colors.green,
-              Colors.limeAccent
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+              const Color.fromARGB(255, 0, 0, 0),
+              const Color.fromARGB(255, 0, 0, 0),
+                const Color.fromARGB(255, 0, 0, 0),
             ])
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipOval(
+                child: Image.asset(
+                    'assets/alert.jpg',
+                    fit: BoxFit.cover,
+                    width: 80,  
+                    height: 80,
+                  ), 
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Active Emergency",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: MediaQuery.of(context).size.width*0.04,
+                    ),
+                    ),
+                    Text("Call 01642468704 for emergencies",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: MediaQuery.of(context).size.width*0.03,
+                    ),
+                    ),
+                    Container(
+                      height: 30,
+                      width: 80,
+                      decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                       color: Colors.white,
+                      ),
+                      child: Text("Call",
+                      textAlign: TextAlign.center ,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: MediaQuery.of(context).size.width*0.045,
+
+                      ) ,),
+                     
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
